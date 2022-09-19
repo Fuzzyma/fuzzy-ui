5e44d646adc5bb85f72f76ff4dbf3edadc836a2d
Fuzzyma <ulima.ums@googlemail.com>
2022-09-19 12:46:34 +0000

    Deploying to gh-pages from @ Fuzzyma/fuzzy-ui@88881af411d6c4e1d69ef6cc68fc9f3ce7337a2e 🚀

--- assets/chunks/DotLoader.2830089e.js
+++
@@ -1,1 +0,0 @@
-import"../index.md.caf6f3c1.js";import{_ as r,o as t,c,b as s,j as e,F as a}from"../app.fb37584b.js";const n={},_=e("circle",{cx:"25",cy:"25",r:"10"},null,-1),l=[_];function d(i,u){return t(),c(a,null,s([0,1,2],o=>e("svg",{key:o,class:"fuzzy-ui-dot-loader",viewBox:"0 0 50 50"},l)),64)}const p=r(n,[["render",d]]);export{p as default};
